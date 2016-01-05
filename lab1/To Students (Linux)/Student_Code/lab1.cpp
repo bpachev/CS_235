6389014d95c091284303ee162c1bbe0965cb9eb0
Benjamin Pachev <benjaminpachev@gmail.com>
2016-01-04 19:30:14 -0700

    Revert "Imported my code."
    This reverts commit 3483915408229e2dcab42eb32831d57966663f38.

--- lab1/To Students (Linux)/Student_Code/lab1.cpp
+++
@@ -1,9 +0,0 @@
-#include "Arena.h"
-
-
-#ifdef MAIN
-int main()
-{
-  return 128;
-}
-#endif
--- lab4/To Students/Student_Code/Node.h
+++
@@ -1,9 +0,0 @@
-template<class T>
-class Node
-{
-  public:
-    Node(T ndata): value(ndata), next(0)
-    {}
-    T value;
-    Node<T>* next;
-};
